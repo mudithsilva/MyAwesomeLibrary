0ad714bc29d95f77244e7cfac094a56a10b57ba0
Mudith Chathuranga <mudithchathuranga@gmail.com>
2018-11-02 16:13:06 +0530

    Added Double extension

---
+++ MyAwesomeLibrary/MyAwesomeLibrary/Double Extension/DoubleExtension.swift
@@ -0,0 +1,21 @@
+//
+//  DoubleExtension.swift
+//  PharmEasy
+//
+//  Created by Mudith Chathuranga on 11/2/18.
+//  Copyright © 2018 Ceffectz. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    
+    func roundedToCurrency() -> String {
+        return String(format: "%.2f", self)
+    }
+}
