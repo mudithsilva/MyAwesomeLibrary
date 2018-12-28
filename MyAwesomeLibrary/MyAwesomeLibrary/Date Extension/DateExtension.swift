a9e3ee78b9c5e4584cb248ae2a4b291399a830e0
Mudith Chathuranga <mudithchathuranga@gmail.com>
2018-12-28 16:23:00 +0530

    Added date extension :- loop through enable

--- MyAwesomeLibrary/MyAwesomeLibrary/Date Extension/DateExtension.swift
+++ MyAwesomeLibrary/MyAwesomeLibrary/Date Extension/DateExtension.swift
@@ -63,3 +63,15 @@ extension Date {
         return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
     }
 }
+
+
+//////  Loop through Date Range  //////
+extension Date: Strideable {
+    public func advanced(by n: Int) -> Date {
+        return Calendar.current.date(byAdding: .day, value: n, to: self) ?? self
+    }
+    
+    public func distance(to other: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: other, to: self).day ?? 0
+    }
+}
